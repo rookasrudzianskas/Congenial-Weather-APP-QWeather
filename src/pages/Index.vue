aa73977f8e2b5b11642f8f6a4d9a514f7c981e9b
Rokas <rookas.rudzenskas@gmail.com>
2021-04-21 18:20:43 +0300

    Rokas

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,7 +8,10 @@
         borderless
         >
         <template v-slot:before>
-          <q-icon name="my_location" />
+          <q-icon
+            name="my_location"
+            @click="getLocation"
+          />
         </template>
 
         <template v-slot:append>
@@ -16,22 +19,41 @@
         </template>
       </q-input>
     </div>
-    <div class="col text-white text-center">
-      <div class="text-h4 text-weight-light">
-        Manchester
-      </div>
-      <div class="text-h6 text-weight-light">
-        Rain
-      </div>
+    <template v-if="weatherData">
+      <div class="col text-white text-center">
+        <div class="text-h4 text-weight-light">
+          Manchester
+        </div>
+        <div class="text-h6 text-weight-light">
+          Rain
+        </div>
 
-      <div class="text-h1 text-weight-thin q-my-lg relative-position">
-        <span>8</span>
-        <span class="text-h4 relative-position degree">&deg;</span>
+        <div class="text-h1 text-weight-thin q-my-lg relative-position">
+          <span>8</span>
+          <span class="text-h4 relative-position degree">&deg;</span>
+        </div>
       </div>
-    </div>
-  <div class="col text-center">
-    <img src="https://www.fillmurray.com/100/100" alt="Bill">
-  </div>
+      <div class="col text-center">
+        <img src="https://www.fillmurray.com/100/100" alt="Bill">
+      </div>
+    </template>
+    <template v-else>
+      <div class="col column text-center text-white">
+        <div class="col text-h2 text-weight-thin">
+          Weather
+        </div>
+
+        <q-btn class="col" flat @click="getLocation">
+          <q-icon
+            left
+            size="3em"
+            name="my_location"
+            />
+          <div>Find my location</div>
+        </q-btn>
+
+      </div>
+    </template>
     <div class="col skyline">
 
     </div>
@@ -44,6 +66,28 @@ export default {
   data() {
     return {
       search: '',
+      weatherData: null,
+      lat: null,
+      lon: null,
+      apiUrl: 'https://api.openweathermap.org/data/2.5/weather',
+      apiKey: '7072edc8ba7df4103c3a2efc6061be00',
+    }
+  },
+  methods: {
+    getLocation() {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position)
+        this.lat = position.coords.latitude
+        this.lon = position.coords.longitude
+        this.getWeatherByCoords()
+
+      })
+    },
+    getWeatherByCoords() {
+      this.$axios(`${ this.apiUrl }?lat=${this.lat}&lon=${this.lon}&appid=${ this.apiKey }&units=metric`).then(response => {
+        this.weatherData = response.data
+
+      })
     }
   }
 }
